637292bb97f50604b7085062fb7f03f249b5af2c
Haukur Thorsteinsson <haukur@haukur.net>
2017-08-13 13:48:42 +0000

    Adjust to new admin look

--- app/overrides/add_base_price_to_product_edit.rb
+++ app/overrides/add_base_price_to_product_edit.rb
@@ -1,14 +1,17 @@
 Deface::Override.new(:virtual_path => 'spree/admin/products/_form',
 	:name => 'add_base_price_to_product_edit',
-	:insert_after => "erb[loud]:contains('text_field :price')",
+	:insert_after => "erb[loud]:contains('data-hook :admin_product_form_price')",
 	:text => "
-		<%= f.field_container :base_price do %>
-			<%= f.label :fill_weight, raw(Spree.t(:fill_weight)) %>
-			<%= f.text_field :fill_weight %>
-			
-			<%= f.label :base_unit, raw(Spree.t(:base_unit)) %>
-			<%= f.text_field :base_unit %>
-			
-			<%= f.error_message_on :base_price %>
-		<% end %>
+		<div data-hook='admin_product_form_cost_price' class='alpha two columns'>
+			<%= f.field_container :base_price, class: ['form-group'] do %>
+				<%= f.label :cost_price, Spree.t(:fill_weight) %>
+				<%= f.text_field :fill_weight, class: 'form-control' %>
+				<%= f.error_message_on :base_price %>
+			<% end %>
+			<%= f.field_container :base_unit, class: ['form-group'] do %>
+				<%= f.label :base_unit, Spree.t(:base_unit) %>
+				<%= f.text_field :base_unit, class: 'form-control' %>
+				<%= f.error_message_on :base_unit %>
+			<% end %>
+		</div>
 	")
