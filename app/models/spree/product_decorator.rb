8a340ed813c256affcf685cc163bd4d99630f6e1
Haukur Thorsteinsson <haukur@haukur.net>
2019-09-26 17:30:00 +0000

    fix delegate syntax

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
 	Product.class_eval do
-		delegate :fill_weight to :master
-		delegate :base_unit to :master
+		delegate :fill_weight, to: :master
+		delegate :base_unit, to: :master
 	end
 end
